26c4d1a12f692591826e3596d61ba4d7b257d91c
Grant Horner <h.grant.horner@gmail.com>
2021-09-19 23:08:28 -0400

    Change annoying helm thing

--- init.el
+++ init.el
@@ -111,7 +111,6 @@
   :diminish helm-mode
   :init
   (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-    helm-move-to-line-cycle-in-source         t
     helm-ff-search-library-in-sexp            t ; search for library in `require' and `declare-function' sexp.
     helm-scroll-amount                        8 ; scroll 8 lines other window using M-<next>/M-<prior>
     helm-ff-file-name-history-use-recentf     t)
